5ba0b8a855e83fb2de65fa692e6ee679bc4fd9cb
Lana <lanadzuceva@gmail.com>
2023-04-11 19:29:43 +0300

    добавила папку с виртуальным окружением, чтобы каждый раз не настраивать и продолжила писать парсер

--- cleaning and union dictionaries.py
+++ cleaning and union dictionaries.py
@@ -28,8 +28,8 @@ def cleaning_digor_rus():
 
 
 class Lexeme:
-    def __init__(self, stem, transl_en, gramm):
-        self.stem = stem
+    def __init__(self, lex, transl_en, gramm):
+        self.stem = lex
         self.transl_en = transl_en
         self.gramm = gramm
 
@@ -39,55 +39,78 @@ class Lexeme:
     def __repr__(self):
         return f'''{self.stem}\n{self.transl_en}\n{self.gramm}\n'''
 
+
 def find_lexem(lexems, word):
     for i in range(len(lexems)):
-        if word in Lexeme(lexems[i]).stem:
+        if word in lexems[i].stem:
             return i
     return -1
 
 
 # TODO: к каждому слову из дигор-русск слов добавить перевод из дигор англ вместе со строками gram
 def union_dictionaries():
-    # [p][i][c][com] грамм категория
-    # для начала распарсю словарь в массив лексем
     lexems = []
     with open('Дигорско-английский очищенный.txt', 'r', encoding='utf-8') as f:
         lines = list(map(lambda x: x.rstrip("\n"), f.readlines()))
         i = 0
-
         while i < len(lines):
             if '-lexeme' in lines[i]:
                 lexems.append(Lexeme(
-                    stem=[i.strip('/').strip('|') for i in lines[i + 2].strip(' stem:').strip('.').split('.')],
+                    lex=[i.strip('/').strip('|') for i in lines[i + 2].strip(' stem:').strip('.').split('.')],
                     transl_en=lines[i + 5].strip(' transl_en:').strip(),
                     gramm=[i.strip() for i in lines[i + 6].strip(' gramm:').split(',')]
                 ))
                 i += 6
             i += 1
-        # for lexem in lexems[:50]:
-        #     print(lexem)
+    with open('Местоимения.txt', 'r', encoding='utf-8') as f:
+        lines = list(map(lambda x: x.rstrip("\n"), f.readlines()))
+        i = 0
+        while i < len(lines):
+            if '-lexeme' in lines[i]:
+                lexems.append(Lexeme(
+                    lex=[i.strip('/').strip('|') for i in lines[i + 2].strip(' stem:').strip('.').split('.')],
+                    transl_en=lines[i + 5].strip(' transl_en:').strip(),
+                    gramm=[i.strip() for i in lines[i + 6].strip(' gramm:').split(',')]
+                ))
+                i += 6
+            i += 1
+
     with open('Дигорско-русский очищенный.txt', 'r', encoding='utf-8') as f:
         lines = list(map(lambda x: x.rstrip("\n"), f.readlines()))
+
     with open('unioned dictionary.txt', 'w', encoding='utf-8') as f:
         i = 0
         while i < len(lines):
             if lines[i].find('[') == -1:
                 f.write(lines[i] + '\n')
                 r = i + 1
-                while lines[r].find('[') != -1:
-                    f.write(lines[r] + '\n')
+                while r < len(lines) and lines[r].find('[') != -1:
+                    if lines[r].strip():
+                        f.write(lines[r] + '\n')
+                    r += 1
                 ind = find_lexem(lexems, lines[i])
-                if ind != -1:
-                    for gr in lexems[ind]:
-                        f.write()
+                if ind != -1 and len(lexems[ind].gramm):
+                    f.write('[m1]')
+                    for gr in lexems[ind].gramm[:-1]:
+                        f.write(f'[p][i][c][com]{gr},[/com][/c][/i][/p]')
+                    f.write(f'[p][i][c][com]{lexems[ind].gramm[-1]},[/com][/c][/i][/p]')
+                    f.write('[/m]\n')
+                    f.write(f'[m1][trn]{lexems[ind].transl_en}[/trn][/m]\n')
+                i = r
+            else:
+                i += 1
 
 
-        for line in lines:
-            if line.find('[') == -1 or \
-                    'm1' in line and ('см.' in line or 'тж.' in line or 'мн.' in line) or \
-                    'm3' in line:
-                f.write(line + '\n')
 
+with open('Дигорско-английский очищенный.txt', 'r', encoding='utf-8') as f:
+    a = sum(map(lambda x: x.rstrip("\n").strip() == "", f.readlines()))
+    print(a)
 
-cleaning_digor_rus()
-# union_dictionaries()
+with open('Местоимения.txt', 'r', encoding='utf-8') as f:
+    a = sum(map(lambda x: x.rstrip("\n").strip() == "", f.readlines()))
+    print(a)
+
+# очистить от пустых строк дигор англ
+# очистить от пустых строк местоимения
+# добавить в список лексем местоимения
+# спросить надо ли делать что-то еще
